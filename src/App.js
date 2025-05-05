aeff50a7c0c4f725a9f14fb66e6702c9aa5514db
nsaimk <nsaimkorkmaz1@gmail.com>
2025-05-05 21:56:08 +0100

    fetches from controller and static folder

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [msg, setMsg] = useState('');
+  const [msg, setMsg] = useState(''); // from controller
+  const [html, setHtml] = useState(''); // from static/index.html
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/ping')
+    fetch('http://localhost:8080')
       .then(res => res.text())
       .then(setMsg)
       .catch(err => console.error('API error:', err));
   }, []);
 
+  useEffect(() => {
+    fetch('http://localhost:8080/index.html')
+      .then(res => res.text())
+      .then(setHtml);
+  }, []);
+
   return (
     <div>
       <h1>Backend says: {msg}</h1>
+      
+        <div dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
 }
